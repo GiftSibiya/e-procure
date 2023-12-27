85c29c467eda89608ec959da0f99b1a3a3eae86d
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-27 12:26:47 +0200

    basic express setup

--- Database/Database.js
+++ Database/Database.js
@@ -1,4 +1,23 @@
+/// Import Dependancies ///
+
 const express = require("express");
-const mongoose = require("mongoose");
+
+//--//
+
+// starting/calling ou express app //
 
 const app = express();
+
+//-//
+
+// This is for form submission
+
+/// The get and post thisngs are here in the router
+app.get("/", (req, res) => {
+  res.json({ Hello: "world" });
+});
+// 300 is our frontend and 4000 is our backend.
+
+app.listen(4000, () => {
+  console.log(`server is running on port 4000`);
+});
