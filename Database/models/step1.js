3d3667b16b722d2cd9bef48345a040a0c57a8ff2
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-29 11:06:52 +0200

    added issue date to schema

--- Database/models/step1.js
+++ Database/models/step1.js
@@ -4,6 +4,7 @@ const tenderSchema = new mongoose.Schema({
   tenderName: String,
   tenderScope: String,
   tenderBid: String,
+  tenderIssue: String,
   tenderClosing: String,
   tenderSessionDate: String,
   tenderVenue: String,
