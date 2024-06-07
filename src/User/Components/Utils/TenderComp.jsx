6e07af3a047588a9794b22c209d2b38a76bf79a4
GiftSibiya <giftsibiya.gb@gmail.com>
2024-06-07 19:42:50 +0200

    Tender Component

--- src/User/Components/Utils/TenderComp.jsx
+++ src/User/Components/Utils/TenderComp.jsx
@@ -3,8 +3,28 @@ import React from 'react'
 export default function TenderComp() {
   return (
     <section className='flex bg-appBlue h-[90px] w-[95%] my-2 rounded-xl '>
-      <div className="w-[70%] h-full">
-        
+      <div className="flex items-center w-[70%] h-full">
+        <div className="rounded-full h-3 w-3 bg-appOrange ml-2"></div>
+        <div className="flex flex-col mx-2 h-full w-[50vw]">
+          <div className="">
+            <h4 className='text-appTextWhite font-semibold'>Tender Name Will Be Placed Here</h4>
+            <div className="flex w-full h-[60px] justify-between">
+              <div className='m-1'>
+                <p className='font-semibold text-appTextWhite'>Advertised Date:</p>
+                <p className='font-semibold text-center'>2024-01-02</p>
+              </div>
+              <div className='m-1'>
+                <p className='font-semibold text-appTextWhite'>Closing Date</p>
+                <p className='font-semibold text-center'>2024-01-02</p>
+              </div>
+              <div className='m-1'>
+                <p className='font-semibold text-appTextWhite text-center'>Status</p>
+                <p className='font-semibold text-center'>2024-01-02</p>
+              </div>
+            
+            </div>
+          </div>
+        </div>
       </div>
       <div className=" w-[30%]"></div>
     </section>
