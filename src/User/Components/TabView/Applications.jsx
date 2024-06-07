b80af82dd6334329b1c00202cbeb1536714cbab8
GiftSibiya <giftsibiya.gb@gmail.com>
2024-06-07 19:26:44 +0200

    login page finals

--- src/User/Components/TabView/Applications.jsx
+++ src/User/Components/TabView/Applications.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TenderComp from "../Utils/TenderComp";
 
 function Applications() {
-  return <div>Applications</div>;
+  return <section className="bg-appGrey mt-2 rounded-lg w-[85vw] h-[58vh]">
+    <div className="flex flex-col items-center">
+      <TenderComp/>
+      <TenderComp/>
+    </div>
+
+  </section>;
 }
 
 export default Applications;
--- src/User/Pages/Intro.jsx
+++ src/User/Pages/Intro.jsx
@@ -9,10 +9,10 @@ export default function Intro() {
           {/*THIS IS THE LEFT SIDE CONTENT */}
 
           <div className="flex items-center w-[50vw] rounded-l-lg bg-appBlue ">
-            <div className="flex-col h-[200px] w-full  border-2 ">
-              <h1 className="text-backgroundColor font-semibold text-[2rem] text-center mb-[10px]">E-PROCUREMENT</h1>
-              <p className="text-center ">You're number 1 electronic procurement solution</p>
-              <p className="text-center items-center ">
+            <div className="flex-col h-[200px] w-full ">
+              <h1 className="text-backgroundColor font-semibold text-[3rem] text-center mb-[10px]">E-PROCUREMENT</h1>
+              <p className="text-center text-[1.2rem] mb-[20px] text-appGrey">You're number 1 electronic procurement solution</p>
+              <p className="text-center items-center text-appTextWhite">
                 Say goodbuy to endless paperwork. <br/>
                 View and create tender RFQ's the
                 21st centuary way</p>
@@ -21,20 +21,20 @@ export default function Intro() {
           {/* */}
 
           {/* Right Side Login In Sheet*/}
-          <div className="flex justift-center items-center h-full w-[45%] border-r-lg bg-appGrey">
-            <div className="flex-col justify-center">
-              <p className="login__text">Free yourself Today</p>
-              <h2 className="logIn">Log in</h2>
-              <form>
+          <div className="flex justify-center items-center h-full w-[45%] border-r-lg bg-appGrey rounded-r-lg">
+            <div className="flex-col justify-center w-[95%]">
+              <p className=" text-center">Free yourself Today</p>
+              <h2 className="text-center">Log in</h2>
+              <form className="flex flex-col justify-center items-center">
                 <textarea
-                  className="resize-none mt-2 w-[30vw]"
+                  className="resize-none mt-2 w-[30vw] rounded-lg m-auto text-center"
                   name="Username"
                   id="Username"
                   placeholder="Username"
                   required
                 ></textarea>
                 <textarea
-                  className="resize-none mt-2 w-[30vw]"
+                  className="resize-none mt-2 w-[30vw] text-center rounded-lg "
                   name="Password"
                   id="Password"
                   placeholder="Password"
@@ -42,38 +42,31 @@ export default function Intro() {
                 ></textarea>
                 <Link to="/user">
                   <button
-                    className="my-3 h-[35px] w-[100px] cursor-pointer border-none bg-appBlue "
-                    type="submit"
-                  >
-                    Log In
-                  </button>
+                    className="my-3 h-[35px] w-[100px] cursor-pointer border-none bg-appBlue text-appTextWhite font-semibold rounded-lg"
+                    type="submit" >Log In</button>  
                 </Link>
               </form>
 
               {/*Login Box */}
-              <div className="flex justify-between">
-                <div className="flex items-center">
-                  <input
-                    className="mr-5 h-3 w-5"
-                    type="checkbox"
-                    name="Name"
-                    id=""
-                  />
-                  <p className="font-5">Remember Me</p>
+              <div className="flex justify-center  ">
+                <div className="flex w-[300px]  items-center justify-between ">
+                  <div className="flex justify-center items-center ">
+                    <input className=" h-3 w-5" type="checkbox" name="Name" id=""/>
+                    <p className="font-5">Remember Me</p>
+                  </div>
+                  <p className="forgot">Forgot Password?</p>
                 </div>
               </div>
               {/* */}
-
-              <p className="forgot">Forgot Password?</p>
-              <div className="flex-col my-5 ">
-                <p className="noAcc__text">Don't Have An Account?</p>
-                <button className="noAcc__btn Button">Sign Up</button>
+              <div className="flex flex-col my-5  items-center">
+                <p className="text-center">Don't Have An Account?</p>
+                <button className="bg-appBlue w-[90px] p-[10px] rounded-md 6">Sign Up</button>
               </div>
             </div>
           </div>
         </div>
-
         {/* */}
+
       </div>
       <div className="NavItems"></div>
     </>
